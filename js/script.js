e3188d2b6cefd2c0ef516fbd1e264f222a47787d
ldm0805 <leonardodimaggio94@gmail.com>
2022-11-25 20:23:04 +0100

    aggiunta commenti

--- js/script.js
+++ js/script.js
@@ -1,30 +1,34 @@
 
 // Chiedere all'utente di inserire il numero di chilometri che vuole percorrere;
 let distance = parseInt(prompt('Inserisci i chilometri che vuoi percorrere'));
-const price_km = 0.21;
-let part_price = (distance * price_km);
-console.log(part_price);
+
 
 // Chiedere all'utente di inserire la sua età;
 let age = parseInt(prompt('Inserisci la tua età'));
-console.log(age);
+
+// Calcolare il prezzo del biglietto in base al prezzo per chilometro (0.21 € al km) e tenendo conto dell'eventuale scontistica nel caso l'età dell'utente soddisfi uno dei seguenti requisiti:
+const price_km = 0.21;
+let part_price = (distance * price_km);
 
 let final_price
-
+// SE L'utente è minorenne
+//         ALLORA ha diritto ad uno sconto del 20%;
+//     SE L'utente ha più di 65 anni 
 if (age < 18){
     final_price = (part_price - (part_price * 0.2));
-    console.log(final_price);
 }
+
+// SE L'utente ha più di 65 anni 
+//         ALLORA ha diritto ad uno sconto del 40%; 
 else if (age >= 65){
     final_price = (part_price - (part_price * 0.4));
-    console.log(final_price);
 }
+// SE L'utente è maggiorenne ma ha meno di 65 anni 
+//         ALLORA non ha diritto allo sconto;
+
 else{
     final_price = part_price;
-    console.log(final_price);
 }
 
-
-
 // Restituire a schermo la password.
-document.getElementById('pippo').innerHTML = 'Il prezzo del biglietto è:' + final_price;
+document.getElementById('pippo').innerHTML = 'Il prezzo del biglietto è:'+ " " + final_price.toFixed(2) +" "+ "&euro;";
